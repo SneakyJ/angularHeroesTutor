efc72a2affa175bec06c43e77bc1cb7839c6a66d
SneakyJ <k.nikolaienko@gmail.com>
2019-08-26 20:03:58 +0200

    added list of heroes. Added css

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id:0, name: 'Dr. Nice' },
+    { id:1, name: 'Dr. IQ' },
+    { id:2, name: 'Dr. Evil' },
+    { id:3, name: 'Bombasto' },
+    { id:4, name: 'Ms. Pepper' },
+    { id:5, name: 'Fire' },
+    { id:6, name: 'Hacker' },
+    { id:7, name: 'Coca-cola diver' },
+    { id:8, name: 'Spritney Birs' },
+    { id:9, name: 'Ellisium' },
+    { id:10, name: 'Colleseus' },
+]
